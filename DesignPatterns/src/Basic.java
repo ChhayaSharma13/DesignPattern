4e5599501529ec075e16b64a674eafe23b22655f
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-08-20 16:08:23 +0100

    User class

---
+++ DesignPatterns/src/Basic.java
@@ -0,0 +1,14 @@
+
+public class Basic {
+	public static void main(String[] args) {
+	 User user = new User("Chhaya");
+	 user.name = "Chhaya";
+	 System.out.println(user.name);
+	 user.sayHello(" World");
+	 
+	 // to avoid tightly coupling use interfaces
+	 //Interface is a contract that specifies the capabilities that a class should provide
+	 
+		
+	}
+}
---
+++ DesignPatterns/src/User.java
@@ -0,0 +1,10 @@
+
+public class User {
+	String name;
+	public User(String name) {
+		this.name = name;
+	}
+	public void sayHello(String name) {
+		System.out.println("Hello" + name);
+	}
+}
