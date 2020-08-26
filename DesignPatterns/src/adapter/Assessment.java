5acaa5c627195082fd6eaea7fa527ef18b21f218
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-08-26 16:21:53 +0100

    Adapter Pattern

---
+++ DesignPatterns/src/adapter/Assessment.java
@@ -0,0 +1,8 @@
+package adapter;
+
+public class Assessment {
+	public static void main(String[] args) {
+		Assignment as = new Assignment();
+		as.writeAsssignment("Writing assignment is a bit boring but its important");
+	}
+}
---
+++ DesignPatterns/src/adapter/Assignment.java
@@ -0,0 +1,20 @@
+package adapter;
+
+public class Assignment implements Pen{
+	private Pen p;
+	
+	public Pen getP() {
+		return p;
+	}
+	public void setP(Pen p) {
+		this.p = p;
+	}
+	public void writeAsssignment(String str) {
+		
+	}
+	@Override
+	public void write(String str) {
+		// TODO Auto-generated method stub
+		
+	}
+}
