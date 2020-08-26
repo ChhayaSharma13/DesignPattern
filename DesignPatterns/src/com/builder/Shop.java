4b1b1a1ca686b6c3b02aa6c0de01b2f6ddb8f505
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-08-26 14:14:30 +0100

    PhoneBuilder class

--- DesignPatterns/src/com/builder/Shop.java
+++ DesignPatterns/src/com/builder/Shop.java
@@ -2,7 +2,9 @@ package com.builder;
 
 public class Shop {
 	public static void main(String[] args) {
-		Phone p = new Phone("Android", 2,"Intel",5.5,3000);
+//		No need to define in parameters in sequence 
+		Phone p = new PhoneBuilder().setOs("Android").setRam(2).setBattery(2000).getPhone();
+//		Phone p = new Phone("Android", 2,"Intel",5.5,3000);
 		System.out.println(p);
 	}
 }
