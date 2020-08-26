cebc700ff633eb71212971591c45fe790d309b8a
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-08-26 23:52:07 +0100

    Composite Test

---
+++ CompositeTest.java
@@ -0,0 +1,7 @@
+package composite;
+
+public class CompositeTest {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ ComputerPart1.java
@@ -0,0 +1,11 @@
+package composite;
+
+public class ComputerPart1 {
+
+}
+class leaf{
+	
+}
+class Composite{
+	
+}
