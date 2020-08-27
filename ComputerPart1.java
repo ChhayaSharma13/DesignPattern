11cd5c52ea16b8676a097b81f5634716783a7e21
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-08-27 09:45:33 +0100

    Component Added

--- ComputerPart1.java
+++ ComputerPart1.java
@@ -1,5 +1,8 @@
 package composite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ComputerPart1 {
 
 }
@@ -7,17 +10,23 @@ interface Component{
 	void showPrice();
 }
 class Leaf implements Component{
-
+	int price;
+	String name;
 	@Override
 	public void showPrice() {
 		// TODO Auto-generated method stub
-		
+		System.out.println(name + " : "+ price);
 	}
 	
 }
 	
 class Composite implements Component{
-
+	String name;
+	List<Component> components = new ArrayList<>();
+	
+	public void addComponent(Component com) {
+		components.add(com);
+	}
 	@Override
 	public void showPrice() {
 		// TODO Auto-generated method stub
