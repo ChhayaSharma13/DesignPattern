f63e070c0df6299078f8cfc8db5291e32589c0b5
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-08-27 22:47:46 +0100

    Calling loadData

--- DesignPatterns/src/prototype/Demo.java
+++ DesignPatterns/src/prototype/Demo.java
@@ -3,6 +3,8 @@ package prototype;
 public class Demo {
 	 public static void main(String[] args) {
 		BookShop bs = new BookShop();
+		bs.setShopName("Novelty");
+		bs.loadData();
 		System.out.println(bs);
 	}
 }
