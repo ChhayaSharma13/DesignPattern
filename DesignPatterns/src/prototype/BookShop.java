52daed2427f76f633de732954604dea260ca533f
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-08-27 22:54:22 +0100

    clone method

--- DesignPatterns/src/prototype/BookShop.java
+++ DesignPatterns/src/prototype/BookShop.java
@@ -3,7 +3,7 @@ package prototype;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BookShop {
+public class BookShop implements Cloneable{
 	private String shopName;
 	List<Book> books = new ArrayList<>();
 	public String getShopName() {
@@ -31,5 +31,13 @@ public class BookShop {
 	public String toString() {
 		return "BookShop [shopName=" + shopName + ", books=" + books + "]";
 	}
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+	
+//	Overriding clone method
+	
 	
 }
--- DesignPatterns/src/prototype/Demo.java
+++ DesignPatterns/src/prototype/Demo.java
@@ -1,10 +1,15 @@
 package prototype;
 
 public class Demo {
-	 public static void main(String[] args) {
+	 public static void main(String[] args) throws CloneNotSupportedException {
 		BookShop bs = new BookShop();
 		bs.setShopName("Novelty");
 		bs.loadData();
 		System.out.println(bs);
+		
+		BookShop bs1 = (BookShop)bs.clone();
+		bs1.setShopName("A1");
+		bs1.loadData();
+		System.out.println(bs1);
 	}
 }
